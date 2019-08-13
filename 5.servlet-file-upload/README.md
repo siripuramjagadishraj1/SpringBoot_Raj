d4c6935dc83d3483618696714ac70fd1605d303e
Jagadish Siripuram <Jagadish_Siripuram@epam.com>
2019-08-13 16:56:47 +0530

    File Upload example

---
+++ 5.servlet-file-upload/README.md
@@ -0,0 +1,3 @@
+Upload 2 jar files..
+1. commons-fileupload-1.4.jar
+2. commons-io-2.6.jar
---
+++ 5.servlet-file-upload/UploadServlet.java
@@ -0,0 +1,91 @@
+
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+
+//Upload 2 jar files..commons-fileupload-1.4.jar, commons-io-2.6.jar
+public class UploadServlet extends HttpServlet {
+	private boolean isMultipart;
+	private String filePath = "D://temp//";
+	private int maxFileSize = 50 * 1024;
+	private int maxMemSize = 4 * 1024;
+	private File file;
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Check that we have a file upload request
+		isMultipart = ServletFileUpload.isMultipartContent(request);
+		response.setContentType("text/html");
+		java.io.PrintWriter out = response.getWriter();
+
+		if (!isMultipart) {
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Servlet upload</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<p>No file uploaded</p>");
+			out.println("</body>");
+			out.println("</html>");
+			return;
+		}
+
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		factory.setSizeThreshold(maxMemSize);
+		// Location to save data that is larger than maxMemSize.
+		factory.setRepository(new File("D:\\temps\\"));
+
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		upload.setSizeMax(maxFileSize);
+
+		try {
+			// Parse the request to get file items.
+			List fileItems = upload.parseRequest(request);
+
+			// Process the uploaded file items
+			Iterator i = fileItems.iterator();
+
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Servlet upload</title>");
+			out.println("</head>");
+			out.println("<body>");
+
+			while (i.hasNext()) {
+				FileItem fi = (FileItem) i.next();
+				if (!fi.isFormField()) {
+					// Get the uploaded file parameters
+					String fieldName = fi.getFieldName();
+					String fileName = fi.getName();
+					String contentType = fi.getContentType();
+					boolean isInMemory = fi.isInMemory();
+					long sizeInBytes = fi.getSize();
+
+					// Write the file
+					if (fileName.lastIndexOf("\\") >= 0) {
+						file = new File(filePath + fileName.substring(fileName.lastIndexOf("\\")));
+					} else {
+						file = new File(filePath + fileName.substring(fileName.lastIndexOf("\\") + 1));
+					}
+					fi.write(file);
+					out.println("Uploaded Filename: " + fileName + "<br>");
+				}
+			}
+			out.println("</body>");
+			out.println("</html>");
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+	
+	}
+
+
+}
